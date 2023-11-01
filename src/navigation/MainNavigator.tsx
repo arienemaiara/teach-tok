869ac18eb6d1e404e55fab9c1f744a1495cbc91d
Ariene Maiara <ariene.maiara@hotmail.com>
2023-11-01 21:19:34 +0100

    feat: configure initial navigation

---
+++ src/navigation/MainNavigator.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { ForYou } from '@/screens'
+
+export default function MainNavigator() {
+  return (
+    <NavigationContainer>
+      <ForYou />
+    </NavigationContainer>
+  )
+}
