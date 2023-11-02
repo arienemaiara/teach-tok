49386036a3708d21e3d95f4445ffe1eb5a077b02
Ariene Maiara <ariene.maiara@hotmail.com>
2023-11-02 11:42:13 +0100

    feat: add layout components

---
+++ src/components/layout/Icon.tsx
@@ -0,0 +1,4 @@
+import styled from 'styled-components/native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+export const StyledIcon = styled(FontAwesomeIcon)``
---
+++ src/components/layout/Texts.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native'
+
+export const DefaultText = styled.Text`
+  letter-spacing: -0.4px;
+  font-family: 'Nunito';
+`
+
+export const HeaderTitle = styled(DefaultText)`
+  font-weight: bold;
+  font-size: 18px;
+`
