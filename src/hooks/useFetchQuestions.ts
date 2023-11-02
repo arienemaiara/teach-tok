61f7e8b1b5ac56e2c5d7f73f17fd5ecc0be0d07f
Ariene Maiara <ariene.maiara@hotmail.com>
2023-11-02 20:29:57 +0100

    feat: avoid repeated ids

--- src/hooks/useFetchQuestions.ts
+++ src/hooks/useFetchQuestions.ts
@@ -1,18 +1,27 @@
+import { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 import { BASE_URL, NUMBER_OF_ITEMS_TO_FETCH } from '@/constants'
 
+import { QuestionType } from '@/types'
+
 export default function useFetchQuestions() {
+  const [fetchedIds, setFetchedIds] = useState<number[]>([])
+
   const fetchQuestion = async () => {
-    const res = await (await fetch(BASE_URL)).json()
+    const res = (await (await fetch(BASE_URL)).json()) as QuestionType
     return res
   }
 
   const fetchQuestions = async () => {
     const collectedData = []
     for (let i = 0; i < NUMBER_OF_ITEMS_TO_FETCH; i++) {
-      const question = await fetchQuestion()
+      let question: QuestionType
+      do {
+        question = await fetchQuestion()
+      } while (fetchedIds.includes(question.id))
       collectedData.push(question)
+      setFetchedIds(prevIds => [...prevIds, question.id])
     }
     return collectedData
   }
