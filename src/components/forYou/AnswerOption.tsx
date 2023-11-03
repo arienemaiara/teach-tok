cdfbc9433346a88906f5917d32cc520866b5615d
Ariene Maiara <ariene.maiara@hotmail.com>
2023-11-03 11:59:00 +0100

    feat: add lottie animation

--- src/components/forYou/AnswerOption.tsx
+++ src/components/forYou/AnswerOption.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components/native'
+import LottieView from 'lottie-react-native'
 
 import { QuestionOptionType } from '@/types'
 import { BoldText } from '../layout/Texts'
+import { LayoutChangeEvent, View } from 'react-native'
+
+const ResultAnimation = ({
+  isRight,
+  itemHeight,
+}: {
+  isRight: boolean
+  itemHeight: number
+}) => {
+  console.log('animation itemHeight', itemHeight)
+  const animationFile = isRight
+    ? require('../../../assets/animations/thumbs_up.json')
+    : require('../../../assets/animations/thumbs_down.json')
+  return (
+    <LottieView
+      source={animationFile}
+      autoPlay
+      loop
+      style={{
+        height: 80,
+        width: 80,
+        position: 'absolute',
+        zIndex: 1,
+        right: 0,
+        top: itemHeight <= 52 ? -15 : 0,
+      }}
+    />
+  )
+}
 
 export default function AnswerOption({
   option,
@@ -15,11 +45,23 @@ export default function AnswerOption({
   rightAnswers: string[]
   onPress: (id: string) => void
 }): JSX.Element {
+  const [itemHeight, setItemHeight] = useState<number>(0)
+  const isRight = rightAnswers.includes(selectedAnswer)
+  const wasSelected = selectedAnswer === option.id
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    setItemHeight(event.nativeEvent.layout.height)
+  }
+
   return (
     <OptionItem
       onPress={() => onPress(option.id)}
-      isRight={rightAnswers.includes(selectedAnswer)}
-      wasSelected={selectedAnswer === option.id}>
+      isRight={isRight}
+      wasSelected={wasSelected}
+      onLayout={handleLayout}>
+      {wasSelected && (
+        <ResultAnimation isRight={isRight} itemHeight={itemHeight} />
+      )}
       <OptionItemText
         style={{
           textShadowColor: 'rgba(0, 0, 0, 1)',
@@ -36,6 +78,7 @@ const OptionItem = styled.TouchableOpacity<{
   isRight: boolean
   wasSelected: boolean
 }>`
+  flex-direction: row;
   min-width: 100%;
   background-color: ${({ wasSelected, isRight }) =>
     !wasSelected ? `#ffffff75` : isRight ? '#00bb4475' : '#a1000075'};
