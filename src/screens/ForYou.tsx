8da2cf2082438d130997bb66bdc550816f7e3634
Ariene Maiara <ariene.maiara@hotmail.com>
2023-11-03 02:30:49 +0100

    refactor: remove unused variables

--- src/screens/ForYou.tsx
+++ src/screens/ForYou.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react'
+import React, { useState, useMemo } from 'react'
 import { Dimensions } from 'react-native'
 import { FlashList } from '@shopify/flash-list'
 
